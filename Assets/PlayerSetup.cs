3dedac608f4cf3b04b55e7aede0dca9fb6e155fe
claudiom4sir <claudio_rota@hotmail.it>
2018-07-30 01:02:14 +0200

    added shoot function

--- Assets/PlayerSetup.cs
+++ Assets/PlayerSetup.cs
@@ -4,24 +4,48 @@ using UnityEngine.Networking;
 public class PlayerSetup : NetworkBehaviour {
 
     public Behaviour[] componentsToDisable;
+    private string remoteLayerName = "RemotePlayer";
     private Camera mainCamera;
+
 
     private void Start()
     {
+        SetPlayerName();
         if (!isLocalPlayer)
-            foreach (Behaviour _componentsToDisable in componentsToDisable)
-                _componentsToDisable.enabled = false;
+        {
+            DisableComponents();
+            AssigneRemotePlayer();
+        }
         else
         {
             mainCamera = Camera.main;
-            mainCamera.gameObject.SetActive(false);
+            if(mainCamera != null)
+                mainCamera.gameObject.SetActive(false);
         }
 
     }
 
+    private void SetPlayerName()
+    {
+        string id = GetComponent<NetworkIdentity>().netId.ToString();
+        gameObject.name = "Player " + id;
+    }
+
     private void OnDisable() // when this player will be destroyed, the main camera will be activated
     {
-        mainCamera.gameObject.SetActive(true);
+        if(mainCamera != null)
+            mainCamera.gameObject.SetActive(true);
+    }
+
+    private void AssigneRemotePlayer()
+    {
+        gameObject.layer = LayerMask.NameToLayer(remoteLayerName);
+    }
+
+    private void DisableComponents() // it disable all components in the list
+    {
+        foreach (Behaviour _componentsToDisable in componentsToDisable)
+            _componentsToDisable.enabled = false;
     }
 
 }
---
+++ Assets/PlayerShoot.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerShoot : NetworkBehaviour {
+
+    public Camera cam;
+    public PlayerWeapon weapon;
+    public LayerMask mask;
+
+    private const string PLAYERTAG = "Player";
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+            Shoot();
+    }
+
+    [Client] // it is used for to say that this method will be invoked only on the client
+    private void Shoot()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, weapon.range, mask))
+            if (hit.collider.tag == PLAYERTAG)
+                CmdPlayerShoot(hit.collider.name);
+    }
+
+    [Command] // it is used for to say that this method will be invoked only on the server
+    private void CmdPlayerShoot(string id)
+    {
+        Debug.Log(id + "hit!");
+    }
+
+}
