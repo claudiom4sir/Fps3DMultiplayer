cb422aa2a06cf50117680c86b07c9a41b19944e5
claudiom4sir <claudio_rota@hotmail.it>
2018-08-11 00:22:34 +0200

    added login menu
    i have not finisched yet to correct the error

---
+++ Assets/UserAccountManager.cs
@@ -0,0 +1,35 @@
+﻿using DatabaseControl;
+using UnityEngine;
+
+public class UserAccountManager : MonoBehaviour {
+
+    public static UserAccountManager singleton;
+    public static string username = "";
+    private static string password = "";
+    public static bool isLoggedIn = false;
+
+    private void Awake()
+    {
+        if(singleton == null)
+        {
+            singleton = this;
+            DontDestroyOnLoad(singleton);
+        }
+        else
+            Destroy(gameObject); // Awake will be called every time we turn on LoginScene
+    }
+
+    public string LogIn(string user, string pass)
+    {
+        username = user;
+        password = pass;
+        isLoggedIn = true;
+    }
+
+    public void Logout()
+    {
+        username = "";
+        password = "";
+        isLoggedIn = false;
+    }
+}
